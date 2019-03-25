9c4f0da24ed151d9b1e5567cddfeb9be8a8994d7
Feel <philipp.bhandari@gmail.com>
2019-03-25 18:06:56 +0300

    Закончен obstacleAt

--- game.js
+++ game.js
@@ -9,13 +9,14 @@ class Vector {
         if(vector instanceof Vector) {
             return new Vector(this.x + vector.x, this.y + vector.y);
         } else {
-            trow('Аргумент функции не является вектором.');
+            throw Error('Аргумент функции не является вектором.');
         }
     }
     times(n) {
         return new Vector(this.x * n, this.y * n);
     }
 }
+
 
 class Actor {
     constructor(position=new Vector(), size=new Vector(1, 1), speed=new Vector()) {
@@ -24,57 +25,160 @@ class Actor {
             && size instanceof Vector
             && speed instanceof Vector) {
 
-            this.pos = position;
             this.size = size;
             this.speed = speed;
 
+            this.pos = position;
+            this.left = this.pos.x;
+            this.right = this.pos.x + this.size.x;
+            this.top = this.pos.y;
+            this.bottom = this.pos.y + this.size.y;
+
         } else {
-            trow('Переданный аргумент не является вектором');
+            throw Error('Переданный аргумент не является вектором');
         }
 
         Object.defineProperty(this, 'type', {
             value: 'actor',
             writable: false
         });
+    }
 
+    get pos(){ return this._pos }
+
+    set pos(value){
+        this._pos = value;
         this.left = this.pos.x;
         this.right = this.pos.x + this.size.x;
         this.top = this.pos.y;
         this.bottom = this.pos.y + this.size.y;
-
     }
+
     act() {
 
     }
+
     isIntersect(actor) {
         if(actor instanceof Actor) {
             switch (true) {
                 case actor === this:
                     return false;
-                // case (this.right > actor.left && (actor.right - this.right - actor.size.x) > 0) || (actor.right > this.left && (actor.right - this.left - actor.left) > 0):
-                case (this.left < actor.left && this.right < actor.left)
-                || (this.left > actor.right && this.right > actor.right):
-                    return false;
-                case (this.right == actor.left || this.left == actor.right
-                    || this.top == actor.bottom || this.bottom == actor.top):
-                    return false;
-                default:
+                case (this.left < actor.left && this.right > actor.left && this.top <= actor.top && this.bottom > actor.top) ||
+                    (actor.left < this.left && actor.right > this.left && actor.top <= this.top && actor.bottom > this.top) ||
+                    (this.left <= actor.left && this.right > actor.left && this.top > actor.top && this.top < actor.bottom) ||
+                    (actor.left <= this.left && actor.right > this.left && actor.top > this.top && actor.top < this.bottom) ||
+                    (this.left === actor.left && this.right === actor.right && this.top === actor.top && this.bottom === actor.bottom):
                     return true;
+                default:
+                    return false;
             }
         } else {
-            trow('Переданный аргумент не является объектом Actor');
+            throw Error('Переданный аргумент не является объектом Actor');
+        }
+    }
+}
+
+
+class Level {
+    constructor(grid=[], actors=[]) {
+        this.height = grid.length || 0;
+        this.width = 0;
+        this.grid = grid;
+        this.actors = actors;
+
+        if(this.grid[0]) { // Вычисление максимальной ширины уровня
+            let cellsLen = this.grid.map(function (cell) {
+                return cell.length;
+            });
+            cellsLen = Math.max.apply(null, cellsLen);
+
+            this.width = cellsLen;
+        }
+
+        this.status = null;
+        this.finishDelay = 1;
+        this.player = new Actor();
+
+    }
+
+    isFinished() {
+        switch (true) {
+            case this.status != null && this.finishDelay < 0:
+                return true;
+            case this.status != null && this.finishDelay > 0:
+                return false;
+            default:
+                return false;
         }
     }
 
-}
+    actorAt(actor) {
+        if(actor instanceof Actor) {
+            if(this.actors === [] || this.actors.length === 1){return undefined;}
 
-class Level {
+            for (let item of this.actors) {
+                if (actor.isIntersect(item)) {
+                    return item;
+                }
+            }
+        } else {
+            throw Error('Переданный аргумент не является объектом Actor');
+        }
+    }
+
+    obstacleAt(vector, size) {
+        if(vector instanceof Vector && size instanceof Vector) {
+            let left = Math.floor(vector.x);
+            let right = Math.ceil(vector.x + size.x);
+            let top = Math.floor(vector.y);
+            let bottom = Math.ceil(vector.y + size.y);
+
+            switch (true) {
+                case left < 0 || right > this.width || top < 0:
+                    return 'wall';
+                case bottom > this.height:
+                    return 'lava';
+            }
+
+            for (let i = top; i < bottom; i++) {
+                for (let j = left; j < right; j++) {
+                    if (this.grid[i][j]) {
+                        return this.grid[i][j];
+                    }
+                }
+            }
+        } else {
+            throw Error('Переданный аргумент не является объектом Actor');
+        }
+    }
+
+
 
 }
 
 const grid = [
-    new Array(3),
-    ['wall', 'wall', 'lava']
+    [undefined, undefined],
+    ['wall', 'wall']
 ];
-const level = new Level(grid);
-runLevel(level, DOMDisplay);
+
+function MyCoin(title) {
+    this.type = 'coin';
+    this.title = title;
+}
+MyCoin.prototype = Object.create(Actor);
+MyCoin.constructor = MyCoin;
+
+const goldCoin = new MyCoin('Золото');
+const bronzeCoin = new MyCoin('Бронза');
+const player = new Actor();
+const fireball = new Actor();
+
+const level = new Level(grid, [ goldCoin, bronzeCoin, player, fireball ]);
+
+
+// const grid = [
+//     new Array(3),
+//     ['wall', 'wall', 'lava']
+// ];
+// const level = new Level(grid);
+// runLevel(level, DOMDisplay);
